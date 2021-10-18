4486b5b7b886190434449b61f902746b4dff51f8
Ezequiel <martinez.ezequiel42@gmail.com>
2021-10-18 11:04:03 -0700

    finished index.js for models

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,8 @@
 const User = require('./User');
+const Article = require('./Article');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const Comment = require('./Comment');
 
-module.exports = { User };
+
+module.exports = { User, Article, Category, Tag, Comment };
