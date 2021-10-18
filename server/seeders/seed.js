18a5d2667abad0eddbc9835fc0dd1d3cd5cd8394
Chris Tolnai <christolnai@gmail.com>
2021-10-18 12:15:03 -0700

    fixed user and article model

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,15 +1,19 @@
 const db = require('../config/connection');
 const { User } = require('../models');
 const { Category } = require('../models')
+const { Article } = require('../models')
 const userSeeds = require('./userSeeds.json');
-const categorySeeds = require('./categoryseeds.json')
+const categorySeeds = require('./categorySeeds.json')
+const articleSeeds = require('./articleSeeds.json')
 
 db.once('open', async () => {
   try {
     await User.deleteMany({});
     await Category.deleteMany({});
+    await Article.deleteMany({});
     await User.create(userSeeds);
     await Category.create(categorySeeds);
+    await Article.create(articleSeeds);
   } catch (err) {
     console.error(err);
     process.exit(1);
