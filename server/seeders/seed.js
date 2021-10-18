6dbe92689640a977d9b8fcdc2b417e0e7f4944da
Chris Tolnai <christolnai@gmail.com>
2021-10-18 11:49:42 -0700

    updated user seeds and category seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,12 +1,15 @@
 const db = require('../config/connection');
 const { User } = require('../models');
+const { Category } = require('../models')
 const userSeeds = require('./userSeeds.json');
+const categorySeeds = require('./categoryseeds.json')
 
 db.once('open', async () => {
   try {
     await User.deleteMany({});
-
+    await Category.deleteMany({});
     await User.create(userSeeds);
+    await Category.create(categorySeeds);
   } catch (err) {
     console.error(err);
     process.exit(1);
