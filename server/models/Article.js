9f3a11938a957ee95af354da14be86be95aed78c
Chris Tolnai <christolnai@gmail.com>
2021-10-18 11:37:20 -0700

    added category seeds file

--- server/models/Article.js
+++ server/models/Article.js
@@ -8,48 +8,48 @@ const articleSchema = new Schema(
       trim: true
     },
     author: {
-        type: String,
-        required: true,
-        trim: true
-      },
-      description: {
-        type: String,
-        required: true,
-        trim: true
-      },
-      thumbnail: {
-        type: String,
-        required: true,
-        trim: true
-      },
-      article_body: {
-        type: String,
-        required: true,
-        trim: true
-      },
-      datePublished: {
-          type: Date,
-          required: true,
-          trim: true
-      },
-      category: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: 'Category'
-        }
-      ],
-      likes: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: 'User'
-        }
-      ],
-      comments: [
-          {
-              type: Schema.Types.ObjectId,
-              ref: 'Comment'
-          }
-      ]
+      type: String,
+      required: true,
+      trim: true
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    article_body: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    datePublished: {
+      type: Date,
+      required: true,
+      trim: true
+    },
+    category: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Category'
+      }
+    ],
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ]
   }
 );
 
