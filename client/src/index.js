58b2681b0180a2796997da1be00e6f2646c536ee
Gerardo Sanchez <oracle942@gmail.com>
2021-10-20 12:21:43 -0700

    Modified navbar

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
-import 'semantic-ui-css/semantic.min.css'
+// import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
 
---
+++ client/src/pages/Profile.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import Nav from '../components/Nav'
+import NavBar from '../components/Navbar'
+
+
+const Profile = () => {
+
+const news = []
+  return (
+      <container>
+      <Nav> </Nav>
+      <h1> User Name</h1>
+    <main>
+        
+      <div >
+       
+      </div>
+    </main>
+  </container>
+  );
+};
+
+export default Home;
+
+
+
