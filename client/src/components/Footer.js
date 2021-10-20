c6d670b2602935dfd566b51e2494eb600db1e9d6
Gerardo Sanchez <oracle942@gmail.com>
2021-10-19 20:06:34 -0700

    Modified footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,8 +2,6 @@ import React from 'react';
 function Footer(){ 
     return (
         <div className = 'footer'>
-            
-        </div>
-    )
-}
+         </div> 
+    )}
 export default Footer
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import '../style.css'
+import { Divider } from 'semantic-ui-react';
 
 const Nav = () => {
     return (
-      <nav> <Link to ="/" className="nav-link">NF<span>Y</span></Link><FontAwesomeIcon icon = {faBars} className ='menu'/></nav>
- 
+      <div>
+      <nav> <Link to ="/" className="nav-link">NF<span>Y</span>
+      </Link><FontAwesomeIcon icon = {faBars} className ='menu-icon'/></nav>
+      </div>
   );
   };
   
