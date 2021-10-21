9316af4ea0610285b537051a214a5cb92712c4f3
Gerardo Sanchez <oracle942@gmail.com>
2021-10-21 07:58:57 -0700

    Added home page header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,28 +1,20 @@
 import React from 'react';
 import { Divider } from 'semantic-ui-react';
-import '../style.css'
+import '../style.css';
 
-
-function Header(){ 
-    return (
-        <section className="p-5 headerbody">
+function Header() {
+  return (
+    <section className="p-5 headerbody">
       <div>
-        
-          <div className="col-md">
-            <div className="card text-dark">
-              <div className="card-body text-center">
-                <div className="h1 mb-3">
-                  <i className="bi bi-person-square"></i>
-                </div>
-                <h3 className="card-title mb-3">News For You</h3>
-              
-              </div>
+        <div className="col-md">
+          <div className="card text-dark">
+            <div className="card-body text-center">
+              <h1 className="card-title mb-3">News For <span> You</span></h1>
             </div>
           </div>
-          
+        </div>
       </div>
     </section>
-
-    )
+  );
 }
-export default Header
+export default Header;
