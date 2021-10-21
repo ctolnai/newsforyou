8dfcd1d99c7445435249f171626084b1a5cd56a8
Gerardo Sanchez <oracle942@gmail.com>
2021-10-21 11:03:15 -0700

    Modal rendering

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -30,15 +30,14 @@ function MyVerticallyCenteredModal(props) {
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
       </Modal.Footer>
+
     </Modal>
   );
 }
 
 
 const NavBar = () => {
-  const modalLoader = () => {
-        return (<MyVerticallyCenteredModal/>)
-  }
+  const [modalShow, setModalShow] = React.useState(false);
     return (
 
 <Navbar collapseOnSelect expand="lg"  bg="dark" variant="dark">
@@ -50,7 +49,14 @@ const NavBar = () => {
     <Nav.Link href="/entertainment">Entertainment</Nav.Link>
     <Nav.Link href="/politics">Politics</Nav.Link>
     <Nav.Link eventKey={2} href="/sports">Sports</Nav.Link>
-    <Nav.Link eventKey={2} onClick = {modalLoader}> Login</Nav.Link>
+    {/* <Nav.Link eventKey={2} onClick = {modalLoader}> Login</Nav.Link> */}
+    <Button variant="primary" onClick={() => setModalShow(true)}>
+        Launch vertically centered modal
+      </Button>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
   </Nav>
 </Navbar.Collapse>
 </Navbar>
