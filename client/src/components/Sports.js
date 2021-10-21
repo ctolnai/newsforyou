a7e706d99be1b5efd4419e6b95e14a071c377ac2
Chris Tolnai <christolnai@gmail.com>
2021-10-21 11:15:45 -0700

    added styling to sports.js cards

--- client/src/components/Sports.js
+++ client/src/components/Sports.js
@@ -1,10 +1,10 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { Card, Button } from 'react-bootstrap/';
 import { useQuery } from '@apollo/client';
 import { QUERY_ARTICLES_CATEGORY } from '../utils/queries';
 
 function Sports() {
-    useEffect(()=>{
+    useEffect(() => {
         console.log(articles)
     })
 
@@ -21,15 +21,18 @@ function Sports() {
 
     return (
         <div>
+            <Card style={{ width: '18rem', height: 'auto', justifyContent: 'center', margin: '15px'}}>
+                <Card.Body style={{backgroundColor: 'red'}}>SPORTS</Card.Body>
+            </Card>
             {articles.map(article => (
-                <Card style={{ width: '18rem' }}>
+                <Card style={{ width: '18rem', height: 'auto', justifyContent: 'center', margin: '15px' }}>
                     <Card.Img variant="top" src={article.thumbnail} />
                     <Card.Body>
                         <Card.Title>{article.title}</Card.Title>
                         <Card.Text>
                             {article.description}
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Button variant="primary">View Full Article</Button>
                     </Card.Body>
                 </Card>
             ))
